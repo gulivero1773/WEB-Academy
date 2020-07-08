2875791372fd61f4c4b2e953552ed8a40d85ada1
Mykola <gulivero1773@gmail.com>
2020-07-08 23:38:12 +0300

    first commit

---
+++ Home work/Home work/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home work
+//
+//  Created by Mykola Buhaiov on 08.07.2020.
+//  Copyright © 2020 Mykola Buhaiov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
